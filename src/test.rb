2e2d057860a18687d25457528efca1a107fc8e99
home <guillaume.derouville@gmail.com>
2020-04-10 12:26:53 +0200

    test - production_ready

--- src/test.rb
+++ src/test.rb
@@ -1,8 +1,11 @@
 require 'tempfile'
 
+PATH="../tests/"
+
 def     create_and_execute_tmp_file(entree, options)
 
     value = ""
+    # Create temp file with entree values
     Tempfile.create { |f| 
         f << entree;
         f.rewind;
@@ -11,33 +14,36 @@ def     create_and_execute_tmp_file(entree, options)
     return value
 end
 
-def     compare_expected_result(path)
+def     compare_expected_result(filename)
 
     # Read the file in one string
-    text = File.read(path)
+    text = File.read(PATH + filename)
     
     # Parse with Regex
     entree = text.scan(/\[Entree\]((.|\n)*?)\[Sortie\]/)[0][0]
     sortie = text.scan(/\[Sortie\]((.|\n)*?)\[options\]/)[0][0].delete("\n")
-    options = text.scan(/\[options\]((.|\n)*)$/)[0][0].delete("\n")
+    options = text.scan(/\[options\]((.|\n)*)$/)[0][0].delete("\n").to_s
 
     # Retrieve execution output
     ret_value = create_and_execute_tmp_file(entree, options).delete("\n")
 
     # Output
     if (sortie == ret_value) then
-        puts "#{path[2..]} -> [\e[32mok\e[0m]"
+        puts "#{filename} -> [\e[32mok\e[0m]"
     else
-        puts "#{path[2..]} -> [\e[31mko\e[0m]"
+        puts "#{filename} -> [\e[31mko\e[0m]"
     end
 
 end
 
 
 def     main()
-
-    compare_expected_result('./format_test')
-
+    
+    # Loop on all tests
+    Dir.foreach(PATH) do |filename|
+        next if filename == '.' or filename == '..'
+        compare_expected_result(filename)
+    end
 end
 
 main()
