cca0d5a651a1722336929fb22d807ae4c053839e
home <guillaume.derouville@gmail.com>
2020-04-10 12:09:35 +0200

    Better test output

--- src/test.rb
+++ src/test.rb
@@ -13,18 +13,22 @@ end
 
 def     compare_expected_result(path)
 
+    # Read the file in one string
     text = File.read(path)
+    
+    # Parse with Regex
     entree = text.scan(/\[Entree\]((.|\n)*?)\[Sortie\]/)[0][0]
     sortie = text.scan(/\[Sortie\]((.|\n)*?)\[options\]/)[0][0].delete("\n")
     options = text.scan(/\[options\]((.|\n)*)$/)[0][0].delete("\n")
+
+    # Retrieve execution output
     ret_value = create_and_execute_tmp_file(entree, options).delete("\n")
 
-    puts entree
-
+    # Output
     if (sortie == ret_value) then
-        puts "The test is a success"
+        puts "#{path[2..]} -> [\e[32mok\e[0m]"
     else
-        puts "The test is an error"
+        puts "#{path[2..]} -> [\e[31mko\e[0m]"
     end
 
 end
